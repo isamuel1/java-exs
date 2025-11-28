619ef3b99df191beb9f8a4e234e61303f5b29af3
Samuel Lopez Mejia <samuelmejialorz@gmail.com>
2025-11-27 19:40:05 -0600

    Se modifo el codigo java

--- AreaCuadrado.java
+++ AreaCuadrado.java
@@ -1,13 +1,7 @@
-import java.util.Scanner;
 public class AreaCuadrado{
-    public static void main(String[] args){
-        Scanner ed = new Scanner(System.in);
 
-        System.out.println("Ingrese el lado del cuadrado: ");
-        double lado = ed.nextDouble();
+public static double computearea(double l1,double l2){
+return l1*l2;
+}
 
-        System.out.println("EL área es: "+Areas.computearea(lado,0));
-        
-        ed.close();
-    }
 }
--- Areas.java
+++ Areas.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 public class Areas{
+    public static void main(String[] args){
+        Scanner ed = new Scanner(System.in);
 
-    public static double computearea(double l, double r){
-        double area=0;
-        if(l > 0) area= l*l;
-        if(r >0 ) area=3.14159 * (r * r);
-     return area;
+        System.out.println("Calculador de areas: \n1.-Cuadrado\n2.-Circulo");
+         
+        int op = ed.nextInt();
+        switch (op) {
+            case 1:
+                System.out.println("Ingrese el lado del circulo: ");
+                double l = ed.nextDouble();
+                System.out.println("El área es: "+AreaCuadrado.computearea(l, l));
+                break;
+            case 2:
+                break;
+            default:
+                System.out.println("Opcion no valida");
+                break;
+        }
+        ed.close();
     }
 }
