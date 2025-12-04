92de0b3eb9336c1c1442cab38135fe285caa311f
Samuel Lopez Mejia <samuelmejialorz@gmail.com>
2025-12-04 12:16:01 -0600

    Se corigio sintaxis

--- AreaCuadrado.java
+++ AreaCuadrado.java
@@ -1,13 +1,7 @@
 public class AreaCuadrado{
-<<<<<<< HEAD
-    public static double computearea(double l){
-        return l*l;
-    }
-}
-=======
 
 public static double computearea(double l){
 return l*l;
 }
 }
->>>>>>> main
+
